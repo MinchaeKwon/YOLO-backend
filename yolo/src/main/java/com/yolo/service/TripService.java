45e80368d0aab29a517a71d4c1b59fb063020d2f
권민채 <minchae075@naver.com>
2021-05-10 10:14:33 +0900

    [update] 필요 없는 코드 삭제, 파라미터 타입 변경

--- yolo/src/main/java/com/yolo/service/TripService.java
+++ yolo/src/main/java/com/yolo/service/TripService.java
@@ -55,7 +55,9 @@ public class TripService {
 	// contenttypeid, addr1, addr2, firstimage1, firstimage2, title
 
 	// 날짜별 여행지 가져오기
-	public String getDateTripInfo(String date, int contentTypeId, int page, String sort) throws IOException, SAXException, ParserConfigurationException {
+	public String getDateTripInfo(String date, Long contentTypeId, int page, String sort) throws IOException, SAXException, ParserConfigurationException {
+		System.out.println(page + ", " + sort);
+		
 		StringBuilder urlBuilder = new StringBuilder(
 				"http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList"); /* URL */
 		urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "="
@@ -64,13 +66,13 @@ public class TripService {
 						"UTF-8")); /* 공공데이터포털에서 발급받은 인증키 */
 		urlBuilder.append(
 				"&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(page), "UTF-8")); /* 현재 페이지 번호 */
-//		urlBuilder.append(
-//				"&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /* 한 페이지 결과 수 */
+		urlBuilder.append(
+				"&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("20", "UTF-8")); /* 한 페이지 결과 수 */
 		urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "="
 				+ URLEncoder.encode("YOLO", "UTF-8")); /* 서비스명=어플명 */
 		urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "="
 				+ URLEncoder.encode("AND", "UTF-8")); /* IOS (아이폰), AND (안드로이드),WIN (원도우폰), ETC */
-	        urlBuilder.append("&" + URLEncoder.encode("arrange","UTF-8") + "=" + URLEncoder.encode(sort, "UTF-8")); /*(A=제목순, B=조회순, C=수정일순, D=생성일순) , 대표이미지가 반드시 있는 정렬 (O=제목순, P=조회순, Q=수정일순, R=생성일순)*/
+//	        urlBuilder.append("&" + URLEncoder.encode("arrange","UTF-8") + "=" + URLEncoder.encode(sort, "UTF-8")); /*(A=제목순, B=조회순, C=수정일순, D=생성일순) , 대표이미지가 반드시 있는 정렬 (O=제목순, P=조회순, Q=수정일순, R=생성일순)*/
 //	        urlBuilder.append("&" + URLEncoder.encode("contentTypeId","UTF-8") + "=" + URLEncoder.encode("15", "UTF-8")); /*관광타입(관광지, 숙박 등) ID*/
 //	        urlBuilder.append("&" + URLEncoder.encode("listYN","UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*목록 구분 (Y=목록, N=개수)*/
 
@@ -97,64 +99,9 @@ public class TripService {
 			map.put("contenttypeId", getTagValue("contenttypeid", eElement));
 			map.put("imageUrl", getTagValue("firstimage", eElement));
 			map.put("thumbnaiUrl", getTagValue("firstimage2", eElement));
-			map.put("mapX", getTagValue("mapx", eElement));
-			map.put("mapY", getTagValue("mapy", eElement));
-			map.put("phone", getTagValue("tel", eElement));
-			map.put("title", getTagValue("title", eElement));
-
-			list.add(map);
-		}
-
-		return test(urlBuilder.toString());
-	}
-	
-	// 지역별 여행지 가져오기
-	public String getAreaTripInfo(int areaCode, int contentTypeId, int page, String sort) throws IOException, SAXException, ParserConfigurationException {
-		StringBuilder urlBuilder = new StringBuilder(
-				"http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList"); /* URL */
-		urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "="
-				+ URLEncoder.encode(
-						"8j34mk+s1/ndx0AkafC8kxGknHpk3HTehopMk9PIig4trbdhrG6PslyubpYwy4UWaU0GpUrcAwAvDsVWJkLi8g==",
-						"UTF-8")); /* 공공데이터포털에서 발급받은 인증키 */
-		urlBuilder.append(
-				"&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(page), "UTF-8")); /* 현재 페이지 번호 */
-//		urlBuilder.append(
-//				"&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /* 한 페이지 결과 수 */
-		urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "="
-				+ URLEncoder.encode("YOLO", "UTF-8")); /* 서비스명=어플명 */
-		urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "="
-				+ URLEncoder.encode("AND", "UTF-8")); /* IOS (아이폰), AND (안드로이드),WIN (원도우폰), ETC */
-	        urlBuilder.append("&" + URLEncoder.encode("arrange","UTF-8") + "=" + URLEncoder.encode(sort, "UTF-8")); /*(A=제목순, B=조회순, C=수정일순, D=생성일순) , 대표이미지가 반드시 있는 정렬 (O=제목순, P=조회순, Q=수정일순, R=생성일순)*/
-//	        urlBuilder.append("&" + URLEncoder.encode("contentTypeId","UTF-8") + "=" + URLEncoder.encode("15", "UTF-8")); /*관광타입(관광지, 숙박 등) ID*/
-	        urlBuilder.append("&" + URLEncoder.encode("areaCode","UTF-8") + "=" + URLEncoder.encode(String.valueOf(areaCode), "UTF-8")); /*지역코드*/
-//	        urlBuilder.append("&" + URLEncoder.encode("listYN","UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); /*목록 구분 (Y=목록, N=개수)*/
-
-		Document documentInfo = null;
-		documentInfo = (Document) DocumentBuilderFactory.newInstance().newDocumentBuilder()
-				.parse(urlBuilder.toString());
-		documentInfo.getDocumentElement().normalize();
-
-		Element root = documentInfo.getDocumentElement();
-		NodeList nList = root.getElementsByTagName("items").item(0).getChildNodes();
-		System.out.println(nList.getLength());
-
-		List<Map<String, String>> list = new ArrayList<>();
-
-		for (int i = 0; i < nList.getLength(); i++) {
-			Map<String, String> map = new HashMap<>();
-			
-			Node nNode = nList.item(i);
-			Element eElement = (Element) nNode;
-
-			map.put("addr1", getTagValue("addr1", eElement));
-			map.put("addr2", getTagValue("addr2", eElement));
-			map.put("contentId", getTagValue("contentid", eElement));
-			map.put("contenttypeId", getTagValue("contenttypeid", eElement));
-			map.put("imageUrl", getTagValue("firstimage", eElement));
-			map.put("thumbnaiUrl", getTagValue("firstimage2", eElement));
-			map.put("mapX", getTagValue("mapx", eElement));
-			map.put("mapY", getTagValue("mapy", eElement));
-			map.put("phone", getTagValue("tel", eElement));
+//			map.put("mapX", getTagValue("mapx", eElement));
+//			map.put("mapY", getTagValue("mapy", eElement));
+//			map.put("phone", getTagValue("tel", eElement));
 			map.put("title", getTagValue("title", eElement));
 
 			list.add(map);
@@ -164,7 +111,8 @@ public class TripService {
 	}
 	
 	// 관광지 상세정보 가져오기 -> 소개/반복/이미지정보/공통정보조회 api 호출 (총 4개)
-	public String getDetail(int contentId, int contentTypeId) {
+	// 여행코스, 숙박 타입의 경우 [반복정보 조회] api 호출 필요
+	public String getDetail(Long contentId, Long contentTypeId) {
 		
 		
 		return null;
