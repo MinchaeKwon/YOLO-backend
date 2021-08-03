b126b51c4aa3afb8127e3e166568a94181955035
권민채 <minchae075@naver.com>
2021-08-03 21:01:51 +0900

    Update WebSecurityConfig.java

--- yolo/src/main/java/com/yolo/security/WebSecurityConfig.java
+++ yolo/src/main/java/com/yolo/security/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/login","/signup", "/email/exist", "/nickname/exist").permitAll().
+				.authorizeRequests().antMatchers("/yolo/login","/yolo/signup", "/yolo/email/exist", "/yolo/nickname/exist").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
