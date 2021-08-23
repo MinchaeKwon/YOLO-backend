45352893b02dbc346d42dbb5d429af8a3f85aa53
권민채 <minchae075@naver.com>
2021-08-23 20:09:27 +0900

    [add] 게시글, 댓글 Entity 추가

---
+++ yolo/src/main/java/com/yolo/entity/Comment.java
@@ -0,0 +1,47 @@
+package com.yolo.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.*;
+
+@Entity
+@Table(name="community_comment")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false)
+	private long id;
+	
+	@Column(name="content")
+	private String content;
+	
+	@Column(name="imageUrl")
+	private String imageUrl;
+	
+	@Column(name="createAt")
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name="updateAt")
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+	
+	@ManyToOne
+	@JoinColumn(name="account_id")
+	private Account account;
+	
+	@Builder
+	public Comment(String content, String imageUrl, Account account) {
+		this.content = content;
+		this.imageUrl = imageUrl;
+		this.account = account;
+	}
+}
---
+++ yolo/src/main/java/com/yolo/entity/Post.java
@@ -0,0 +1,51 @@
+package com.yolo.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.*;
+
+@Entity
+@Table(name="community_post")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false)
+	private long id;
+	
+	@Column(name="content")
+	private String content;
+	
+	@Column(name="imageUrl")
+	private String imageUrl;
+	
+	@Column(name="createAt")
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name="updateAt")
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+	
+	@ManyToOne
+	@JoinColumn(name="account_id")
+	private Account account;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="post", orphanRemoval=true)
+	private List<Comment> comment;
+	
+	@Builder
+	public Post(String content, String imageUrl, Account account) {
+		this.content = content;
+		this.imageUrl = imageUrl;
+		this.account = account;
+	}
+}
