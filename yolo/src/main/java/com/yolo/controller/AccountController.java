0a9eeddc5c0fe790506d77b6ce80784d3c815170
권민채 <minchae075@naver.com>
2021-09-03 23:31:30 +0900

    [add] 사용자 프로필 사진 삭제 api 추가

--- yolo/src/main/java/com/yolo/controller/AccountController.java
+++ yolo/src/main/java/com/yolo/controller/AccountController.java
@@ -1,10 +1,12 @@
 package com.yolo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +49,7 @@ public class AccountController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body(new ErrorResponse("회원가입을 하는 도중 오류가 발생했습니다.", "500"));
 		}
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(new Response("회원가입을 성공적으로 완료했습니다."));
 	}
 
@@ -62,35 +64,35 @@ public class AccountController {
 			userDetails = userDetailService.loadUserBySocialIdAndType(request.getSocialId(), request.getType());
 
 			token = jwtTokenUtil.generateToken(userDetails);
-			
+
 		} catch (SocialUserNotFoundException e) {
 			// 문자열로 보내지말고 int로 1234 이런식으로 보내기
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("일치하는 회원정보가 없습니다.", "404"));
 		}
-		
+
 		return ResponseEntity.ok(new JwtResponse(token));
 	}
-	
+
 	// 로그인한 사용자의 정보 가져오기
 	@GetMapping(value = "/account/profile")
 	public ResponseEntity<?> getMyAccount(@AuthenticationPrincipal Account account) {
 		Image image = account.getImage();
 		String imageUrl = null;
-		
+
 		if (image != null) {
 			imageUrl = image.getImageUrl();
 		}
-		
-		AccountDto.Profile profile = new AccountDto.Profile(account.getSocialId(), account.getType(), 
+
+		AccountDto.Profile profile = new AccountDto.Profile(account.getSocialId(), account.getType(),
 				account.getNickname(), imageUrl);
 		return ResponseEntity.ok().body(new SuccessResponse<AccountDto.Profile>(profile));
 	}
-	
+
 	// 사용자 id로 정보 가져오기
 	@GetMapping(value = "/account/{id}")
 	public ResponseEntity<?> getMyAccountById(@PathVariable("id") Long accountId) {
 		AccountDto.Profile account;
-		
+
 		try {
 			account = userDetailService.loadUserById(accountId);
 		} catch (Exception e) {
@@ -99,30 +101,50 @@ public class AccountController {
 
 		return ResponseEntity.ok().body(new SuccessResponse<AccountDto.Profile>(account));
 	}
-	
+
 	// 회원정보 수정
 	@PutMapping("account/profile")
-	public ResponseEntity<?> updateAccount(@RequestBody AccountUpdateDto infoDto, @AuthenticationPrincipal Account account) {
+	public ResponseEntity<?> updateAccount(@RequestBody AccountUpdateDto infoDto,
+			@AuthenticationPrincipal Account account) {
 		try {
 			userDetailService.updateAccount(infoDto, account);
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new ErrorResponse("회원정보 수정 실패", "500"));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("회원정보 수정 실패", "500"));
 		}
-		
+
 		return ResponseEntity.ok().body(new Response("회원정보 수정 성공"));
 	}
-	
+
+	@DeleteMapping("account/profile/image")
+	public ResponseEntity<?> deleteRecipePost(@RequestParam String imageUrl) {
+		boolean result;
+
+		try {
+			result = userDetailService.deleteImage(imageUrl);
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("일치하는 이미지가 없습니다.", "404"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("이미지 삭제 실패", "500"));
+		}
+
+		if (result) {
+			return ResponseEntity.ok().body(new Response("이미지 삭제 성공"));
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("이미지 삭제 실패", "500"));
+		}
+
+	}
+
 	// 닉네임 중복 확인 -> 사용 X
 	@GetMapping("/nickname/exist")
-	public ResponseEntity<?> isExistName(@RequestParam("nickname") String nickname){		
-		if(userDetailService.isExistNickname(nickname)) {
+	public ResponseEntity<?> isExistName(@RequestParam("nickname") String nickname) {
+		if (userDetailService.isExistNickname(nickname)) {
 			return ResponseEntity.ok().body(new Response("이미 존재하는 닉네임입니다."));
-		}
-		else {
+		} else {
 			return ResponseEntity.ok().body(new Response("사용 가능한 닉네임입니다."));
 		}
 	}
-	
-	
+
 }
